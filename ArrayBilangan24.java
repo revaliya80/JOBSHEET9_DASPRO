fe88fd5303f253eb83ff974704cdd906d2ef997c
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-05 22:41:38 +0700

    pertanyaan 2

---
+++ ArrayBilangan24.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayBilangan24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] bil = { 5.0, 12867, 7.5, 2000000 };
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+
+    }
+}
