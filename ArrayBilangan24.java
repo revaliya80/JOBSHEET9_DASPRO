15cc3cf7ad7d25f31ff336b38d189028c49e9ff2
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-05 22:43:17 +0700

    pertanyaan 3

--- ArrayBilangan24.java
+++ ArrayBilangan24.java
@@ -6,10 +6,9 @@ public class ArrayBilangan24 {
 
         double[] bil = { 5.0, 12867, 7.5, 2000000 };
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
 
     }
 }
