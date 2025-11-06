f2d8bff1ea013be071284f1c22adfd1f3d810884
Refaliya-Vinandha <revaliya80@gmail.com>
2025-11-06 10:00:12 +0700

    Tugas 3

---
+++ Tugas3Cafe.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Tugas3Cafe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] menu = { "Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino",
+                "Chocolate Ice" };
+
+        System.out.println("=== Daftar Menu Cafe ===");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println((i + 1) + ". " + menu[i]);
+        }
+
+        System.out.println("Masukkan nama makanan yang ingin dicari : ");
+        String cari = sc.nextLine();
+
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i].equalsIgnoreCase(cari)) {
+                System.out.println("Menu" + cari + " tersedia di kafe.");
+            } else {
+                System.out.println("Makanan yang dicari tidak ada di menu");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
